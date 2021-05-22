23fe76b839b7e9ae98945b0841a3e502fbfbab7d
kushwanth3103 <63758858+kushwanth3103@users.noreply.github.com>
2021-05-22 12:18:15 +0530

    updated

--- public/events.js
+++ public/events.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
-    var eventsList=[{title:"Event 1",club:"Club 1",desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Consectetur nisi, tempor pharetra id viverra. Urna sit porttitor arcu consectetur tincidunt feugiat a risus eleifend.",link:"",image:"",tags:["app","web"]},{title:"Event 2",club:"Club 2",desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Consectetur nisi, tempor pharetra id viverra. Urna sit porttitor arcu consectetur tincidunt feugiat a risus eleifend.",link:"",image:"",tags:["app","web"]},{title:"Event 3",club:"Club 3",desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Consectetur nisi, tempor pharetra id viverra. Urna sit porttitor arcu consectetur tincidunt feugiat a risus eleifend.",link:"",image:"",tags:["app","web"]}]
-    for(var i=0;i<eventsList.length;i++){
-        var div="<div id=eventitem class=row>"
-        var image="<img src=home.png class=col-sm-2>"
-        var tags="<div id=desc class=col-sm-8><br>"
-        for(var j=0;j<eventsList[i].tags.length;j++){
-            tags=tags+"<span class=eventtag>"+eventsList[i].tags[j]+"</span>"
-        }
-        var title="<br><p style=padding-top:10px>"+eventsList[i].title+"</p>"
-        var club="<p>"+eventsList[i].club+"</p>"
-        var desc="<p>"+eventsList[i].desc+"</p></div>"
-        var button="<div class=col-sm-2 ><a><button style=background-color:#023E8A;color:#FFFFFF>View More</button></a></div></div>"
-        $("#eventslist").append(div+image+tags+title+club+desc+button)
+    $.ajax('/events',
+    {
+        type:'GET',
+        dataType:'json',
+        success:function(data1,status,xhr)
+        {
+            for(var i=0;i<data1.length;i++){
+                console.log(data1[i]._id)
+                var div="<div id=eventitem class=row>"
+                var image=""
+                var tags="<div id=desc class=col-sm-8><br>"
+                var arr_strings=data1[i].tags
+                var arr=arr_strings.split(',')
+                for(var j=0;j<arr.length;j++)
+                {
+                    tags=tags+"<span class=eventtag>"+arr[j]+"</span>"
+                }
+                var title="<br><p style=padding-top:10px>"+data1[i].name+"</p>"
+                var club="<p>"+data1[i].name+"</p>"
+                var desc="<p>"+data1[i].description+"</p></div>"
+                var button="<div class=col-sm-2 ><a><button style=background-color:#023E8A;color:#FFFFFF class='but' value='"+data1[i]._id+"'>View More</button></a></div></div>"
+                $("#eventslist").append(div+image+tags+title+club+desc+button);
+            }
+        },
 
-    }
-
-})
+    });
+    
+});

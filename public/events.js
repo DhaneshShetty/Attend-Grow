45bfb3d6d63bd47fd64ff0c230c097dcc7d1b4ba
Tanish <tanishmalekar32@gmail.com>
2021-05-29 22:42:24 +0530

    minor bug

--- public/events.js
+++ public/events.js
@@ -86,26 +86,12 @@ function displayAll(){
       {
         console.log("Success ajax");
           for(var i=0;i<data1.length;i++){
-
               displayItem(data1[i]);
           }
       },
   });
 }
 
-$(document).ready(function(){
-    console.log($("#start-date-input").val());
-    displayAll();
-
-    $('#name-input').keyup(function(event) {
-        if (event.which === 13)
-        {
-            $("#eventslist").empty();
-            $("#search-button").html("❌");
-            searchName();
-        }
-    });
-  });
 
 function hexToBase64(str) {
     return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
@@ -113,38 +99,16 @@ function hexToBase64(str) {
 
 $(document).ready(function(){
 
-    $.ajax('../events/events',
-    {
-        type:'GET',
-        dataType:'json',
-        success: function(data1,status,xhr)
-        {
-          console.log("Success ajax");
-            for(var i=0;i<data1.length;i++){
-                var div="<div id='eventitem' class='row'>";
-                var img_data = bytesToBase64(data1[i].img.data.data);
-								var image="<img class='eventItemImg' src='data:" + data1[i].img.contentType + ";base64," + img_data + "' alt='Event Poster'>";
-                var tags="<div id='desc' class='col-sm-8'><br>";
-                var arr_strings=data1[i].tags
-                var arr=arr_strings.split(',')
-                for(var j=0;j<arr.length;j++)
-                {
-                    tags=tags+"<span class=eventtag>"+arr[j]+"</span>"
-                }
-                var title="<br><p style=padding-top:10px>"+data1[i].name+"</p>"
-                //var club="<p>"+data1[i].name+"</p>"
-                var club = "";
-                var desc="<p>"+data1[i].description+"</p></div>";
-								var button="<form action='event.html' method='get'>"
-													+	"<input type='hidden' name='event_id' value='" + data1[i]._id + "'>"
-													+ "<input type='submit' value='View More'> </form>"
+  displayAll();
 
-								$("#eventslist").append(div+image+tags+title+club+desc+button);
-            }
-        },
-      });
-
-
+  $('#name-input').keyup(function(event) {
+      if (event.which === 13)
+      {
+          $("#eventslist").empty();
+          $("#search-button").html("❌");
+          searchName();
+      }
+  });
 
     $("#search-button").click(function(){
       if(!($("#name-input").val()==="")){
@@ -153,11 +117,11 @@ $(document).ready(function(){
           $("#search-button").html("❌");
           searchName();
         }
-        else if($("#search-button").html()=="❌"){
-          $("#eventslist").empty();
-          $("#search-button").html("🔍");
-          displayAll();
-        }
+      }
+      if($("#search-button").html()=="❌"){
+        $("#eventslist").empty();
+        $("#search-button").html("🔍");
+        displayAll();
       }
     });
 
@@ -171,4 +135,4 @@ $(document).ready(function(){
       searchDate();
     });
 
-    });
+});

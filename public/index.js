e6bb6c37650adc2b053defb1e900ba7196ae802f
Dhanesh Shetty <dhaneshshetty65@gmail.com>
2021-05-30 23:13:17 +0530

    UI updates

--- public/index.js
+++ public/index.js
@@ -13,47 +13,7 @@ $(document).ready(function(){
         }
     },130);
     
-    function getCookie(cookieName) {
-        var name = cookieName + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i].trim();
-            if ((c.indexOf(name)) == 0) {
-                return true;
-            }    
-        }
-        return false;
-    }
-    if(getCookie('loggedIn')){
-        $('#login_logout').text("Log Out");
-    }
-    else{
-        $('#login_logout').text("Log In/SignUp");
-    }
-    $('#login_logout').click(function(){
-        if(getCookie('loggedIn')){
-            $.ajax('/users/logout',{
-                type:'GET',
-                dataType:'json',
-                success:function(data1,status,xhr){
-                    if(getCookie('loggedIn')){
-                        $('#login_logout').text("Log Out");
-                    }
-                    else{
-                        $('#login_logout').text("Log In/SignUp");
-                    }
-                },
-                error:function(xhr,status,error){
-                    console.log("Error:"+error);
-                    console.log("Status:"+status);
-                }
-            });
-            
-        }
-        else{
-            $('#login_logout').attr('href','/login.html');
-        }
-    });
+    
     $.ajax('/events/events?count=3',{
         type:'GET',
         dataType:'json',
